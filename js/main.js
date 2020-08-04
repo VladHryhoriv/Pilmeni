e2ca2664ede5bab517059bbb7d6be2ce055e256f
VladHr <hryhorivvlad1@gmail.com>
2020-08-04 22:19:40 +0300

    Start work with double slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  let number = 1;
+
+  const to_basket = document.getElementById("to_basket");
+  const count = document.querySelector(".count")
+  const count_number = document.querySelector(".count__number")
+  const plus = document.querySelector(".plus")
+  const minus = document.querySelector(".minus")
+
+  console.log(to_basket)
+
+  plus.addEventListener("click", function () {
+    ++number;
+    console.log(number)
+    count_number.innerHTML = number;
+  });
+
+  minus.addEventListener("click", function () {
+    
+    console.log(number)
+    if (number-1 <= 0) {
+      to_basket.classList.remove("not-active");
+      count.classList.toggle("not-active");
+    }
+    else {
+      number--;
+    count_number.innerHTML = number;
+    }
+  });
+
+  to_basket.addEventListener("click", function () {
+    console.log("===============")
+    console.log(to_basket)
+    this.classList.toggle("not-active");
+    count.classList.remove("not-active");
+  });
+    
+});
+
 $(document).ready(function(){
   $('.main__slider').slick({
     centerMode: true,
@@ -26,3 +66,4 @@ $(document).ready(function(){
     ]
   });
 });
+
