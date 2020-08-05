2a5c625a4d29048a2e6af01910cac4c282daae9e
Vlad Hryhoriv <hryhorivvlad1@gmail.com>
2020-08-05 15:46:11 +0300

    Adaptive 940 done

--- js/main.js
+++ js/main.js
@@ -4,13 +4,43 @@ $(document).ready(function(){
     centerPadding: '150px',
     slidesToShow: 1,
     dots: true,
+    arrows : false,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 940,
+        settings: {
+          centerPadding: '0',
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '0',
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+$(document).ready(function(){
+  $('.users-slider').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1,
+    dots: true,
+    arrows : false,
+    responsive: [
+      {
+        breakpoint: 940,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
           slidesToShow: 1
         }
       },
