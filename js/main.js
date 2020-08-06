9cec8de8f64fce8fda05a3041982c00a06a6630f
Vlad Hryhoriv <hryhorivvlad1@gmail.com>
2020-08-06 18:54:49 +0300

    Start do menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
   const lang = document.querySelector(".menu__list");
   const burger = document.querySelector(".burger-menu");
+  const width = document.documentElement.clientWidth;
+    console.log(width)
+  if(width <= 480 ) {
+    const store = document.querySelector(".store").classList.toggle('single-item');
+    $(document).ready(function(){
+      $('.single-item').slick({
+        responsive: [
+          {
+            breakpoint: 481,
+            settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow: 1,
+                dots: true,
+                prevArrow:' <button type = "button" class = "slick-prev"> <img src="./img/prev.svg" class="next" alt="" srcset=""> </ button>',
+                nextArrow: '<button type = "button" class = "slick-next"> <img src="./img/next.svg" class="next" alt="" srcset=""> </ button>'
+              }
+            }
+        ]
+      });
+    });
+    
+  }
 
   burger.addEventListener("click", function () {
     this.classList.toggle("active");
     this.classList.toggle("not-active");
-    menu.classList.toggle("header__nav_active");
-    body.classList.toggle("active");
+    document.querySelector(".menu").classList.remove('menu__not-active')
+    document.querySelector(".menu").classList.toggle('menu__active')
   });
   lang.addEventListener("click", function () {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -36,7 +60,16 @@ $(document).ready(function(){
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '40px',
+          centerPadding: '0',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0',
           slidesToShow: 1
         }
       }
@@ -65,7 +98,7 @@ $(document).ready(function(){
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '40px',
+          centerPadding: '0',
           slidesToShow: 1
         }
       }
